ce2f9e52369a3d0f2e6c405c987755da31aac55c
Dima Lagutin <fsss70178@gmail.com>
2019-04-29 23:11:31 +0400

    add features

--- src/js/app.js
+++ src/js/app.js
@@ -14,6 +14,7 @@ import '../blocks/pagination/pagination.scss';
 import '../blocks/rate/rate.scss';
 import '../blocks/bulletList/bulletList.scss';
 import '../blocks/review/review.scss';
+import '../blocks/features/features.scss';
 
 import '../blocks/sumGuests/sumGuests';
 import '../blocks/range/range';
