fc0ef73a79ab2a2d57579e7fb44f4e6b5e9fbe96
Dima Lagutin <fsss70178@gmail.com>
2019-10-07 00:46:42 +0400

    range slider bem refactoring, #7

--- src/blocks/ui-slider/ui-slider.js
+++ src/blocks/ui-slider/ui-slider.js
@@ -1,15 +1,15 @@
 require('jquery-ui/ui/widgets/slider');
 require('jquery-ui/themes/base/slider.css');
 
-const $sliderElement = $('.js-slider');
-const $labelElement = $('.js-range__label');
+const $sliderElement = $('.ui-slider__range');
+const $labelElement = $('.ui-slider__label');
 
 $sliderElement.slider({
   range: true,
-  min: 1000,
-  max: 50000,
+  min: 0,
+  max: 16000,
   step: 1000,
-  values: [1000, 50000],
+  values: [5000, 10000],
   slide: (event, ui) => {
     $labelElement.html(`${ui.values[0].toString()
       .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}&#8381; - ${ui.values[1].toString()
